224f01a61e9c4b690565c73d5d32f57cc19fe31f
zahid dehara <zahiddehara@zahids-MacBook-Pro.local>
2016-04-09 22:44:08 -0500

    toggle

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ $(document).ready(function(){
     var products = [];
     var $check = $('.myCheckbox');
     for (var i = 0; i < 3; i++) {
-      if($check[i].checked == true) {
+      if($check[i].checked == false) {
         products.push($check[i].value);
       }
     }
     console.log(products);
+  });
+
+  $('#button-see-products').click(function(e) {
+    e.preventDefault();
+    $('.checkboxes').toggle();
   });
 
 });
